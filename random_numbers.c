27cfb8f65498b9fb9e2d543fd22f695002356a2d
Chin-Chopa <Shusha225@yandex.ru>
2020-08-31 19:50:08 +0700

    add function shuffle

--- random_numbers.c
+++ random_numbers.c
@@ -3,3 +3,14 @@ void make_arr(int* a, int size)
     for(int i = 0; i < size; i++)
         a[i] = i + 1;
 }
+
+void shuffle(int* chisla, int N )
+{
+    srand(time(NULL));
+    for (int i = N - 1; i >= 1; i--)
+    {
+        int j = rand() % (i + 1);
+        int tmp = chisla[j];
+        chisla[j] = chisla[i];
+        chisla[i] = tmp;
+    }
