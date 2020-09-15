cfc1b88ef5803ba013526d2643b77c5d7b1dcc08
Chin-Chopa <Shusha225@yandex.ru>
2020-09-15 09:41:38 +0700

    add funtcion read_verb

--- verb_functions.c
+++ verb_functions.c
@@ -1,1 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "read_verbs.h"
+#include <time.h>
 
+void read_verb(IrrVerb *ans, FILE *f, int line)
+{
+    int nlcounter = 0;
+    while(nlcounter != (line - 1))
+    {
+        if (fgetc(f) == '\n')
+            nlcounter += 1;
+    }
+    for(int i = 0 ; i < 3; i++)
+        fscanf(f, "%s", ans->verb[i]);
+    fseek(f, 0, SEEK_SET);
+}
