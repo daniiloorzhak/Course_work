c6b25378fa70945426687ca3671fe849b2111bda
daniiloorzhak <daniiloorzhak@gmail.com>
2020-09-27 00:59:15 +0700

    headers written

--- random_numbers.h
+++ random_numbers.h
@@ -1,1 +1,6 @@
 
+#ifndef RANDOM_NUMBERS_H
+#define RANDOM_NUMBERS_H
+    void make_arr(int* a, int size);
+    void shuffle(int* chisla, int N );
+#endif
--- verb_functions.h
+++ verb_functions.h
@@ -1,3 +1,8 @@
+#ifndef VERB_FUNCTIONS_H
+#define VERB_FUNCTIONS_H
     typedef struct {
         char verb[3][14];
     }IrrVerb;
+    int check_answer(IrrVerb *corr,char *s1,char *s2);
+    
+#endif
