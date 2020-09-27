989fa3ddd1eae5ed65c214abc04554ae8b381933
daniiloorzhak <daniiloorzhak@gmail.com>
2020-09-27 23:37:46 +0700

    added #includes added cyrillic

--- src/irr_verbs.c
+++ src/irr_verbs.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "random_numbers.h"
+#include "verb_functions.h"
+#include "irrverbs.h"
 
 void program_meaning()
 {
@@ -5,42 +11,44 @@ void program_meaning()
     printf(" knowledge of irregular verbs\n");
 }
 
-//"скелет" программы
+
+
+//"СЃРєРµР»РµС‚" РїСЂРѕРіСЂР°РјРјС‹
 int irrverbs(IrrVerb *correct)
 {
-FILE *a = fopen("IV.txt","r");
-if(a==NULL) {
-printf("Cannot open file IV.txt\n");
-return -1;
-}
-float score=0;//кол-во правильных ответов
-int point;//1 если ответ правильный, 0 в ином случае
-int mas[200];//массив случайных неповторяющихся чисел
-int k;//случайное число
-make_arr(mas, 200);
-shuffle(mas, 200);
-for(int i = 0 ; i < 40; i++)
-{
-IrrVerb asd;
-correct = &asd;
-point=0;
-char str1[15],str2[15];
-k = mas[i];//случайное число
-read_verb(correct,a,k);
-printf("%s: ",correct->verb[0]);
-scanf("%s%s", str1, str2);
-point=check_answer(correct, str1, str2);
-score += point;
-printf(" Your score is %.0f of 40\n", score);
-if(point==0)
-{
-printf("The right forms of this verb are: ");
-printf("%s %s ",correct->verb[0],correct->verb[1]);
-printf("%s\n",correct->verb[2]);
-}
-}
-float result;
-result = (score / 10) * 100;
-printf("Percentage of correct answers: %.0f%%\n",result);
-return 0;
-}
+    FILE *a = fopen("src/IV.txt","r");
+    if(a==NULL) {
+        printf("Cannot open file IV.txt\n");
+        return -1;
+    }
+    float score=0;//РєРѕР»-РІРѕ РїСЂР°РІРёР»СЊРЅС‹С… РѕС‚РІРµС‚РѕРІ
+    int point;//1 РµСЃР»Рё РѕС‚РІРµС‚ РїСЂР°РІРёР»СЊРЅС‹Р№, 0 РІ РёРЅРѕРј СЃР»СѓС‡Р°Рµ
+    int mas[200];//РјР°СЃСЃРёРІ СЃР»СѓС‡Р°Р№РЅС‹С… РЅРµРїРѕРІС‚РѕСЂСЏСЋС‰РёС…СЃСЏ С‡РёСЃРµР»
+    int k;//СЃР»СѓС‡Р°Р№РЅРѕРµ С‡РёСЃР»Рѕ
+    make_arr(mas, 200);
+    shuffle(mas, 200);
+    for(int i = 0 ; i < 40; i++)
+    {
+        IrrVerb asd;// 
+        correct = &asd;
+        point=0;
+        char str1[15],str2[15];
+        k = mas[i];//СЃР»СѓС‡Р°Р№РЅРѕРµ С‡РёСЃР»Рѕ
+        read_verb(correct,a,k);
+        printf("%s: ",correct->verb[0]);
+        scanf("%s%s", str1, str2);
+        point=check_answer(correct, str1, str2);
+        score += point;
+        printf("    Your score is %.0f of 40\n", score);
+        if(point==0)
+        {   
+            printf("    The right forms of this verb are: ");
+            printf("%s %s ",correct->verb[0],correct->verb[1]);
+            printf("%s\n",correct->verb[2]);
+        }
+    }
+    float result;
+    result = (score / 10) * 100;
+    printf("Percentage of correct answers: %.0f%%\n",result);
+    return 0;
+}    
