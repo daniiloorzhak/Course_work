ca867ae3856658e6cf70085b614f3e77d775e3e0
daniiloorzhak <61111863+daniiloorzhak@users.noreply.github.com>
2020-09-28 18:08:52 +0700

    Delete irr_verbs.h

--- src/irr_verbs.h
+++
@@ -1,8 +0,0 @@
-#ifndef IRRVERBS_H
-#define IRRVERBS_H
-    #include "verb_functions.h"
-    #include <stdio.h>
-    void program_meaning();
-    int irrverbs(IrrVerb *correct);
-    void read_verb(IrrVerb *ans, FILE *f, int line);
-#endif
