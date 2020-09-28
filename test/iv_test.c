b06df38d55734f8b410f1c853cb9f3ef75311d1d
daniiloorzhak <61111863+daniiloorzhak@users.noreply.github.com>
2020-09-28 18:06:20 +0700

    Update iv_test.c

--- test/iv_test.c
+++ test/iv_test.c
@@ -18,15 +18,3 @@ CTEST(suite1, test1){
     ASSERT_STR("beat", irrverb->verb[1]);
     ASSERT_STR("beaten", irrverb->verb[2]);
 }
-
-//make_arr
-CTEST(suite, test2)
-{
-//ASSERT_DATA(exp, expsize, real, realsize)
-    int *exp = malloc(sizeof(int) * 200);
-    for(int i = 0; i < 200; i++)
-        exp[i] = i + 1;
-    int *real = malloc(sizeof(int) * 200); 
-    make_arr(real, 200);
-    ASSERT_DATA(exp, 200, real, 200);
-}
