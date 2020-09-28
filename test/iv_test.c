6eb1c842d8bcd59ec26b93c36f94fdf8c2caef46
daniiloorzhak <daniiloorzhak@gmail.com>
2020-09-29 00:39:08 +0700

    new tests

--- test/iv_test.c
+++ test/iv_test.c
@@ -6,8 +6,8 @@
 #include "random_numbers.h"
 #include "verb_functions.h"
 
-//irrverbs
-CTEST(suite1, test1){
+//readverb
+CTEST(read_verb, test1){
     char* expected = "beat";
     IrrVerb* irrverb = malloc(sizeof(IrrVerb));
     FILE* cur = fopen("src/IV.txt","r");
@@ -17,4 +17,28 @@ CTEST(suite1, test1){
     ASSERT_STR(expected, real);
     ASSERT_STR("beat", irrverb->verb[1]);
     ASSERT_STR("beaten", irrverb->verb[2]);
+}
+
+//make_arr
+CTEST(make_arr, test1){
+    int numbers[200];
+    make_arr(numbers, 200);
+    for(int i = 0; i < 200; i++)
+        ASSERT_EQUAL(i + 1, numbers[i]);
+}
+
+//shuffle
+CTEST(shuffle, test1){
+    int numbers1[200];
+    int numbers2[200];
+    make_arr(numbers1, 200);
+    make_arr(numbers2, 200);
+    shuffle(numbers1, 200);
+    shuffle(numbers1, 200);
+    int counter = 0;
+    for(int i = 0; i < 200; i++){
+        if(numbers1[i] == numbers2[i])
+            counter++;
+    ASSERT_NOT_EQUAL(200, counter);
+    }
 }
